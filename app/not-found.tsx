24852af5d02ca0001db6d864aac9446b57876df7
Eazweb <eazwebcreates@gmail.com>
2025-02-10 09:25:46 +0530

    kdnkfsld

---
+++ app/not-found.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from 'react';
+import Container from './components/container';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Container>
+        <div className="h-[60vh] flex flex-col gap-4 items-center justify-center">
+          <h2 className="text-2xl font-bold">404 - Page Not Found</h2>
+          <p>Could not find the requested resource</p>
+          <Link 
+            href="/"
+            className="text-slate-500 underline hover:text-slate-800"
+          >
+            Return Home
+          </Link>
+        </div>
+      </Container>
+    </Suspense>
+  );
+} 
